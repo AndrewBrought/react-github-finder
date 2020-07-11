303b77d6ef1b859ac7294fc4e11b98bee42b1462
Andrew Brought <andrewbrought@gmail.com>
2020-07-11 15:10:09 -0500

    greated reducer and actions

--- src/App.js
+++ src/App.js
@@ -41,18 +41,7 @@ const App = () => {
     // }
 
     // NEW - Search Github users
-    const searchUsers = async text => {
-        setLoading(true );
 
-        const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
-
-
-        //OLD
-        // this.setState({ users: res.data.items , loading: false });
-        //New
-        setUsers(res.data.items);
-        setLoading(false);
-    };
 
     //Get single Github user
     const getUser = async username => {
@@ -104,7 +93,6 @@ const App = () => {
                    render={props => (
                    <Fragment>
                        <Search
-                            searchUsers={searchUsers}
                             clearUsers={clearUsers}
                             showClear={users.length > 0 ? true : false}
                             setAlert={showAlert}
--- src/context/github/GithubState.js
+++ src/context/github/GithubState.js
@@ -20,6 +20,20 @@ const GithubState = props => {
     const [state, dispatch] = useReducer(GithubReducer, initialState);
 
     //Search Users
+    const searchUsers = async text => {
+        setLoading();
+
+        const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+
+        //OLD
+        // this.setState({ users: res.data.items , loading: false });
+
+        //New
+       dispatch({
+           type: SEARCH_USERS,
+           payload: res.data.items
+       });
+    };
 
     // Get User
 
@@ -28,13 +42,16 @@ const GithubState = props => {
     // Clear Users
 
     // Set Loading
+    const setLoading = () => dispatch({ type: SET_LOADING });
+
 
     return <GithubContext.Provider
         value={{
             users: state.users,
             user: state.user,
             repos: state.repos,
-            loading: state.loading
+            loading: state.loading,
+            searchUsers
         }}
         >
         {props.children}
